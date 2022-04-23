0fd2b86d43624c28506272b98484acaec91738e5
DESKTOP-1NFSJND\daronlee <13237443239@163.com>
2022-04-23 20:06:30 +0800

    * complete http service of zset

---
+++ router_zset.go
@@ -0,0 +1,38 @@
+package nutshttp
+
+func (s *NutsHTTPServer) initZSetRouter() {
+	sr := s.r.Group("/zset")
+
+	sr.POST("/zadd/:bucket/:key", s.SAdd)
+
+	sr.GET("/zcard/:bucket/:key", s.ZCard)
+
+	sr.GET("/zcount/:bucket/:key", s.ZCount)
+
+	sr.GET("/zgetbykey/:bucket/:key", s.ZGetByKey)
+
+	sr.GET("/zmembers/:bucket/:key", s.ZMembers)
+
+	sr.GET("/zpeekmax/:bucket/:key", s.ZPeekMax)
+
+	sr.GET("/zpeekmin/:bucket/:key", s.ZPeekMin)
+
+	sr.DELETE("/zpopmax/:bucket/:key", s.ZPopMax)
+
+	sr.DELETE("/zpopmin/:bucket/:key", s.ZPopMin)
+
+	sr.GET("/zrangebyrank/:bucket/:key", s.ZRangeByRank)
+
+	sr.GET("/zrangebyscore/:bucket/:key", s.ZRangeByScore)
+
+	sr.GET("/zrank/:bucket/:key", s.ZRank)
+
+	sr.GET("/zrevrank/:bucket/:key", s.ZRevRank)
+
+	sr.DELETE("/zrem/:bucket/:key", s.ZRem)
+
+	sr.GET("/zremrangebyrank/:bucket/:key", s.ZRemRangeByRank)
+
+	sr.GET("/zscore/:bucket/:key", s.ZScore)
+
+}
